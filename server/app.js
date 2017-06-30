887b76d37e57270ec251a13f4e3e80249c629386
Jackson Tenclay <jptenclay@gmail.com>
2017-06-30 13:28:52 -0500

    app.js

---
+++ server/app.js
@@ -0,0 +1,28 @@
+var express = require("express"),
+	app = express(),
+	server = require("http").createServer(app),
+	session = require("express-session"),
+	path = require("path");
+
+require("./db/db");
+
+app.use(session({
+	secret: "hashdashhh",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {secure: false}
+}));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+
+
+
+
+
+
+server.listen(3000, function(){
+	console.log("Listening on 3000!!")
+});
