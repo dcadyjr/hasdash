4798fb2c04ead4ae3ede3d2051c7eaac8dafcb41
Jackson Tenclay <jptenclay@gmail.com>
2017-07-03 11:23:25 -0500

    added login and logout

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -11,29 +11,68 @@ var express = require("express"),
 
 router.use(bodyParser.urlencoded({extended: true}));
 
+// get login page
+router.get("/login", function(req, res) {
+	res.render("login");
+});
+
+// log out
+router.get("/logout", function(req, res) {
+	req.session.loggedIn = false;
+	res.render("logout");
+});
+
 router.get("/register", function(req, res) {
 	res.render("register");
 });
 
-router.get("/", function(req, res) {
-	User.find(function(err, users) {
-		var renderObject = {users: users};
+// login with information
+router.post("/login", function(req, res) {
+	User.findOne({email: req.body.email}, function(err, user) {
+		if (user) {
+			bcrypt.compare(req.body.password, user.password, function(err, match) {
+				if (match === true) {
+					req.session.loggedIn = true;
+					req.session.myId = user._id;
+					res.redirect("/users/" + user._id);
+				} else {
+					res.send("password incorrect"); // password was wrong
+				}
+			});
+		} else {
+			res.send("couldn't find that email address") // email not found
+		};
+	});
+});
 
-		twitter.getSearch({'q':'#cubs','count': 10}, function(){} , function(data){
-			obj = JSON.parse(data);
-			// console.log(obj.statuses[0].extended_entities);
-			var results = tweetToHTML.parse(obj.statuses);
-			console.log(results[0].html);
-			res.send(data);
-		});
-		// res.render("userlist", renderObject);
-	})
+router.get("/", function(req, res) {
+	if (req.session.loggedIn === true) {
+		User.find(function(err, users) {
+			var renderObject = {
+				users: users,
+				session: req.session
+			};
+
+			twitter.getSearch({'q':'#cubs','count': 10}, function(){} , function(data){
+				obj = JSON.parse(data);
+				// console.log(obj.statuses[0].extended_entities);
+				var results = tweetToHTML.parse(obj.statuses);
+				console.log(results[0].html);
+				res.send(data);
+			});
+			// res.render("userlist", renderObject);
+		})
+	} else {
+		res.redirect("/");
+	};
 });
 
 // just for testing since GET to /users/ no longer lists all the users
 router.get("/listofusers", function(req, res) {
 	User.find(function(err, users) {
-		var renderObject = {users: users};
+		var renderObject = {
+			users: users
+		};
 		res.json(renderObject);
 	});
 });
@@ -41,7 +80,10 @@ router.get("/listofusers", function(req, res) {
 router.get("/:id", function(req, res) {
 	if (req.session.loggedIn === true) {
 		User.findById(req.params.id).populate("hashtags").exec(function(err, user) {
-			var renderObject = {user: user};
+			var renderObject = {
+				user: user,
+				session: req.session
+			};
 			res.render("dashboard", renderObject)
 		});
 	} else {
--- server/public/js/main.js
+++ server/public/js/main.js
@@ -19,3 +19,23 @@ $("#sign-up").click(function() {
 		}
 	});
 });
+
+// post to /login to login
+$("#sign-up").click(function() {
+	var email = $("#email-field").val();
+	var name = $("#name-field").val();
+	var password = $("#password-field").val();
+	var newUser = {
+		email: email,
+		name: name,
+		password: password
+	};
+	$.ajax({
+		method: "POST",
+		url: "http://localhost:3000/users",
+		data: newUser,
+		success: function(response) {
+			window.location.href = "/users/" + response;
+		}
+	});
+});
