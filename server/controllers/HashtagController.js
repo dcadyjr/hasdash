318ececece735f0d2c0413363544449bff34a321
David Cady Jr <davidcadyjr@mac.com>
2017-07-02 21:28:15 -0500

    not able to get ajax working. error has some thing to do iwth not recognizing jquery I think

--- server/controllers/HashtagController.js
+++ server/controllers/HashtagController.js
@@ -4,9 +4,47 @@ var express = require("express"),
 	Hashtag = require("../models/Hashtag"),
 	User = require("../models/User"),
 	session = require("express-session"),
-	bcrypt = require("bcrypt");
+	bcrypt = require("bcrypt"),
+	twitter = require("../twitter_api.js"),
+	tweetToHTML = require('tweet-to-html');
+
+// var jsdom = require("jsdom").jsdom;
+// jsdom.env("", function(err, window) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     global.$ = require("jquery")(window);
+// })	
 
 router.use(bodyParser.urlencoded({extended: true}));
+
+router.get("/search", function(req, res){
+
+	twitter.getSearch({'q':'#cubs','count': 10}, function(){} , function(data){//this is the search to get tweet data.
+			
+		tweets = JSON.parse(data);//this allows us to dig into the tweet data
+
+		for (var i = 0; i < tweets.statuses.length; i++){//this loops through each tweet and grabs the userScreenName and tweetId
+
+			var userScreenName = tweets.statuses[i].user.screen_name;//variable to hold the UserScreenName
+			var tweetId = tweets.statuses[i].id_str;//variable to hold the tweet Id
+			
+			$.ajax({
+				method: "GET",
+				url: "https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2Fedent%2Fstatus%2F860905721646338048",
+				success: function(response){
+					console.log(response);
+				}	
+			});
+
+			console.log(userScreenName);
+			console.log(tweetId);
+			
+		}
+			res.send(data);
+	})
+})
 
 router.get("/", function(req, res) {
 	Hashtag.find(function(err, hashtags) {
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -12,29 +12,41 @@ var express = require("express"),
 
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.get("/", function(req, res) {
-	User.find(function(err, users) {
-		var renderObject = {users: users};
+router.get("/search", function(req, res){
 
-		twitter.getSearch({'q':'#cubs','count': 10}, function(){} , function(data){
+	twitter.getSearch({'q':'#cubs','count': 10}, function(){} , function(data){//this is the search to get tweet data.
 			
-			tweets = JSON.parse(data);
+			tweets = JSON.parse(data);//this allows us to dig into the tweet data
 
-			for (var i = 0; i < tweets.statuses.length; i++){
+			for (var i = 0; i < tweets.statuses.length; i++){//this loops through each tweet and grabs the userScreenName and tweetId
 
-			var userScreenName = tweets.statuses[i].user.screen_name;
-			var tweetId = tweets.statuses[i].id_str;
+			var userScreenName = tweets.statuses[i].user.screen_name;//variable to hold the UserScreenName
+			var tweetId = tweets.statuses[i].id_str;//variable to hold the tweet Id
 			
+				$.ajax({
+					method: "GET",
+					url: "https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2Fedent%2Fstatus%2F860905721646338048",
+					success: function(response){
+						console.log(response);
+					}	
+				});
+
+
 			console.log(userScreenName);
 			console.log(tweetId);
 			
 			}
 			res.send(data);
 		})
-		
+})
 
-		// res.render("userlist", renderObject);
 
+router.get("/", function(req, res) {
+
+	User.find(function(err, users) {
+		var renderObject = {users: users};
+
+		res.render("userlist", renderObject);
 	})
 
 });
