0518a389d1142d3ea3179ee42a97f9cfcd654957
Jackson Tenclay <jptenclay@gmail.com>
2017-07-06 16:03:22 -0500

    sidebar links preventDefault

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -27,6 +27,7 @@ var $grid = $('.masonry-grid').masonry({
 // get tweets via ajax so the user doesn't go to a new address every time
 
 var requestHashtag = function(hashtag) {
+	$(".grid-item").remove();
 	var tag = hashtag
 	var userId = $("body").attr("id");
 	var data = {
@@ -72,7 +73,8 @@ $("#get-tweets-button").click(function(){
 
 
 // get tweets when you click on a hashtag in the sidebar
-$(".taglist").click(function() {
+$(".taglist").click(function(e) {
+	e.preventDefault();
 	var hashtag = $(this).text();
 	requestHashtag(hashtag);
 	console.log(hashtag)
@@ -154,54 +156,4 @@ $("#account-submit-button").click(function() {
 		alert("password is required to change account info");
 	};
 });
-
-//click function to initiate search from history
-$(".taglist").click(function(){
- 
-	
-	var embedHTML = [];
-
-	twitter.getSearch({'q': "#" + $(this).text(),'count': 5}, function(){} , function(data){//this is the search to get tweet data.
-			
-		tweets = JSON.parse(data);//this allows us to dig into the tweet data
-
-		for (var i = 0; i < tweets.statuses.length; i++){//this loops through each tweet and grabs the userScreenName and tweetId
-
-			var userScreenName = tweets.statuses[i].user.screen_name;//variable to hold the UserScreenName
-			var tweetId = tweets.statuses[i].id_str;//variable to hold the tweet Id
-		
-			//this url sends us code to embed the tweet on our page
-			var url = 'https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2F' + userScreenName + '%2Fstatus%2F' + tweetId;
-
- 			request(url, function(err, resp, body){//request embed tweet info from twitter
- 				if(err){
- 					// console.log(err);
- 				}else {
-
- 				var embedCode = JSON.parse(body);//this allows us to dig into the tweet imbed data
- 					
- 					embedHTML.push(embedCode.html);
-
- 					if (embedHTML.length === 5){
- 					var html = {tweets: embedHTML};
-					res.render("dashboard", html);
-				}
-				
- 				}
- 			})
-		}
-			
-	})
-});
-
-
-
-
-
-
-
-
-
-
-
 
